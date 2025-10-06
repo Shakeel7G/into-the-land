de0a4b8e6b8ab1e3afbebdf0d6b86d3e7d3caa58
Shakeel Gafieldien <shakeel.gafieldien@gmail.com>
2025-10-06 09:09:24 +0200

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@
 // It sets the base URL for the API and configures it to not send credentials by default
 import axios from 'axios';
 
-const BASE = process.env.VUE_APP_BACKEND_URL || 'http://localhost:4000'; // this allows the base URL to be set via an environment variable, defaulting to localhost if not set
+const BASE = process.env.VUE_APP_BACKEND_URL || 'https://into-the-land-backend.onrender.com'; // this allows the base URL to be set via an environment variable, defaulting to localhost if not set
 // This is useful for development and production environments where the backend URL may differ 
 
 console.log('Backend URL is:', process.env.VUE_APP_BACKEND_URL);
